c39a8d6f828877e7655d92cd77d78baeabc79e87
Frank Pak <fpak210@gmailc.om>
2017-04-02 19:15:17 -0700

    add icons to buttons

--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {withRouter} from 'react-router';
-import {Image, Button} from 'react-bootstrap';
+import {Image, Button, Glyphicon} from 'react-bootstrap';
 import GreetingContainer from '../greeting/greeting_container';
 
 class Sidebar extends React.Component {
@@ -23,19 +23,25 @@ class Sidebar extends React.Component {
 
         <ul className="sidebar-button-container">
           <li>
-            <Button bsStyle="success" onClick={e => this.handleClick(e, '/note/new')}>New Note</Button>
+            <Button bsStyle="success" onClick={e => this.handleClick(e, '/note/new')}><Glyphicon glyph="plus" />
+              New Note
+            </Button>
           </li>
 
           <li>
-            <Button bsStyle="success" onClick={e => this.handleClick(e, '/notebooks')}>Notebooks</Button>
+            <Button bsStyle="success" onClick={e => this.handleClick(e, '/notebooks')}><Glyphicon glyph="book" />
+              Notebooks
+            </Button>
           </li>
 
           <li>
-            <Button bsStyle="success" onClick={e => this.handleClick(e, '/search/tags')}>Search Tags</Button>
+            <Button bsStyle="success" onClick={e => this.handleClick(e, '/search/tags')}><Glyphicon glyph="search" />
+              Search Tags
+            </Button>
           </li>
         </ul>
 
-        <GreetingContainer className="greeting-container"/>
+        <GreetingContainer />
       </div>
 
     )
